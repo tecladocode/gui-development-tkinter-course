2a4e7c974679a6d3fb73ee8f233d387074dd59e2
Phil Best <me@phillip.best>
2019-03-06 11:58:40 +0100

    First commit

---
+++ section01/hello_world/app.py
@@ -0,0 +1,13 @@
+# Implementation in Tcl/Tk
+#
+# package require Tk
+# grid [ttk::button .b -text "Hello, World!"]
+
+# In Python 3, the correct import is tkinter not Tkinter. Tkinter was the name of the module used for Python 2
+from tkinter import *
+from tkinter import ttk    # ttk is the Python binding to the newer "themed widgets" added in Tk version 8.5
+
+root = Tk()
+ttk.Button(root, text = "Hello, World!").grid()
+
+root.mainloop()
