f9f6420ebf9eb8de2fe6db38c75a0bec8d597d28
Phil Best <me@phillip.best>
2019-03-10 21:54:17 +0100

    Adds the main layout for the second distance converter frame.

--- test_projects/distance_converter/distance_converter_02.py
+++ test_projects/distance_converter/distance_converter_02.py
@@ -5,6 +5,7 @@ class DistanceConverter(Tk):
 
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
+
         self.title("Distance Calculator")
 
         container = ttk.Frame(self)
@@ -16,7 +17,6 @@ class DistanceConverter(Tk):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row = 0, column = 0, sticky = (N, S, E, W))
-
         
         self.show_frame(MetresToFeet)
 
@@ -64,6 +64,29 @@ class MetresToFeet(Frame):
 class FeetToMetres(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
+
+        self.feet_value = StringVar()
+        self.metres_value = StringVar()
+
+        feet_label = ttk.Label(self, text = "feet")
+        feet_label.grid(column = 1, row = 1, sticky = W, ipadx = 5)
+
+        metres_label = ttk.Label(self, text = "metres")
+        metres_label.grid(column = 1, row = 2, sticky = W, ipadx = 5)
+
+        feet_input = ttk.Label(self, textvariable = self.feet_value)
+        feet_input.grid(column = 2, row = 1, sticky = (E, W))
+
+        metres_display = ttk.Entry(self, width = 10, textvariable = self.metres_value)
+        metres_display.grid(column = 2, row = 2, sticky = (E, W))
+
+        calculate_button = ttk.Button(self, text = "Calculate", command = self.calculate_metres)
+        calculate_button.grid(column = 1, row = 3, columnspan = 2, sticky = (E, W))
+
+        for child in self.winfo_children():
+            child.grid_configure(padx = 5, pady = 5)
+
+        feet_input.focus()
 
     def calculate_metres(self, *args):
         pass
