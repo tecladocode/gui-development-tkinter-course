afe43e63aefee92234fa06dd6efa5b238ebd81a9
Phil Best <me@phillip.best>
2019-04-05 02:26:37 +0200

    Corrects some minor layout issues in greeter_2.py

--- section03/greeter_2/app.py
+++ section03/greeter_2/app.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 def greet(*args):
     name = user_name.get()
     greeting_message.set(f"Hello, {name or 'World'}!")
-    greeting.grid(row=2, column=1, columnspan=2, sticky="W", pady=(10, 0))
+    greeting.grid(row=1, column=0, columnspan=2, sticky="W", pady=(10, 0))
 
 
 root = tk.Tk()
@@ -14,28 +14,30 @@ root.title("Greeter")
 user_name = tk.StringVar()
 greeting_message = tk.StringVar()
 
-frame = ttk.Frame(root, padding=(20, 10, 20, 0))
-frame.grid()
+main = ttk.Frame(root, padding=(20, 10, 20, 0))
+main.grid()
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(1, weight=1)
 
-name_label = ttk.Label(frame, text="Name:")
-name_label.grid(row=1, column=1, padx=(0, 10))
+name_label = ttk.Label(main, text="Name:")
+name_label.grid(row=0, column=0, padx=(0, 10))
 
-name_entry = ttk.Entry(frame, width=15, textvariable=user_name)
-name_entry.grid(row=1, column=2)
+name_entry = ttk.Entry(main, width=15, textvariable=user_name)
+name_entry.grid(row=0, column=1)
 
-greeting = ttk.Label(frame, textvariable=greeting_message)
+greeting = ttk.Label(main, textvariable=greeting_message)
 
-buttons = ttk.Frame(frame, padding=(0, 10))
-buttons.grid(row=3, column=1, columnspan=2, sticky="EW")
+buttons = ttk.Frame(main, padding=(0, 10))
+buttons.grid(row=2, column=0, columnspan=2, sticky="EW")
+buttons.columnconfigure(0, weight=1)
+buttons.columnconfigure(1, weight=1)
 
 greet_button = ttk.Button(buttons, text="Greet", command=greet)
-greet_button.grid(row=1, column=1, sticky="W")
+greet_button.grid(row=0, column=0, sticky="EW")
 
 quit_button = ttk.Button(buttons, text="Quit", command=root.destroy)
-quit_button.grid(row=1, column=2, sticky="E")
+quit_button.grid(row=0, column=1, sticky="EW")
 
 root.bind("<Return>", greet)
 root.bind("KP_Enter", greet)
