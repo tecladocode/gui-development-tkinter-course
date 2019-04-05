aa9facce0daf2bf906f447eaf5cd15922ac7e719
Phil Best <me@phillip.best>
2019-04-05 02:30:20 +0200

    Minor revisions to the distance converter grid implementation.

--- section04/distance_converter/app_grid.py
+++ section04/distance_converter/app_grid.py
@@ -20,21 +20,21 @@ main = ttk.Frame(root, padding="30 15 30 15")
 main.grid()
 
 root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=1)
 
 metres_label = ttk.Label(main, text="metres")
-metres_label.grid(column=1, row=1, sticky="W", padx=5, pady=5)
+metres_label.grid(column=0, row=0, sticky="W", padx=5, pady=5)
 metres_input = ttk.Entry(main, width=10, textvariable=metres_value)
-metres_input.grid(column=2, row=1, sticky="EW", padx=5, pady=5)
+metres_input.grid(column=1, row=0, sticky="EW", padx=5, pady=5)
 metres_input.focus()
 
 feet_label = ttk.Label(main, text="feet")
-feet_label.grid(column=1, row=2, sticky="W", padx=5, pady=5)
+feet_label.grid(column=0, row=1, sticky="W", padx=5, pady=5)
 feet_display = ttk.Label(main, textvariable=feet_value)
-feet_display.grid(column=2, row=2, sticky="EW", padx=5, pady=5)
+feet_display.grid(column=1, row=1, sticky="EW", padx=5, pady=5)
 
 calc_button = ttk.Button(main, text="Calculate", command=calculate_feet)
-calc_button.grid(column=1, row=3, columnspan=2, sticky="EW", padx=5, pady=5)
+calc_button.grid(column=0, row=2, columnspan=2, sticky="EW", padx=5, pady=5)
 
 root.bind("<Return>", calculate_feet)
 root.bind("<KP_Enter>", calculate_feet)
